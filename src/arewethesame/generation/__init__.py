d0982c4ef098161580440011df403ea391a89579
Khagendra Khatri <52613596+Khagendra01@users.noreply.github.com>
2026-09-11 22:43:53 -0700

    Expose assistant curation workflow

--- src/arewethesame/generation/__init__.py
+++ src/arewethesame/generation/__init__.py
@@ -1,4 +1,20 @@
+from .assistant_batch import (
+    AssistantAudit,
+    AssistantAuditTask,
+    AssistantBatchBuilder,
+    AssistantRender,
+    AssistantRenderTask,
+)
 from .build_dataset import NaturalizedDatasetBuilder, RenderedRow
 from .splitter import split_for_life
 
-__all__ = ["NaturalizedDatasetBuilder", "RenderedRow", "split_for_life"]
+__all__ = [
+    "AssistantAudit",
+    "AssistantAuditTask",
+    "AssistantBatchBuilder",
+    "AssistantRender",
+    "AssistantRenderTask",
+    "NaturalizedDatasetBuilder",
+    "RenderedRow",
+    "split_for_life",
+]
