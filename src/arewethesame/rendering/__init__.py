817449ba97f13abf718de1c6b8eff358ff8e4e4c
Khagendra Khatri <52613596+Khagendra01@users.noreply.github.com>
2026-09-11 17:55:40 -0700

    Expose v0.3 rendering API

---
+++ src/arewethesame/rendering/__init__.py
@@ -0,0 +1,5 @@
+from .canonical import CanonicalRenderer, CanonicalScene
+from .perspective import PerspectiveRenderer
+from .styles import STYLE_NAMES
+
+__all__ = ["CanonicalRenderer", "CanonicalScene", "PerspectiveRenderer", "STYLE_NAMES"]
