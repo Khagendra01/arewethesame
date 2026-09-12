035c7348addade48cf00c66940a88c5875f74aff
Khagendra Khatri <52613596+Khagendra01@users.noreply.github.com>
2026-09-11 17:41:58 -0700

    Add world simulator package

---
+++ src/arewethesame/world/__init__.py
@@ -0,0 +1,5 @@
+from .state import RichLifeState, RelationshipState
+from .events import CausalEvent
+from .simulator import LifeSimulator
+
+__all__ = ["RichLifeState", "RelationshipState", "CausalEvent", "LifeSimulator"]
