0aae5191f9937ab837fbe3cba398ec8236456da3
Khagendra Khatri <52613596+Khagendra01@users.noreply.github.com>
2026-09-11 17:42:50 -0700

    Add evaluation package

---
+++ src/arewethesame/evals/__init__.py
@@ -0,0 +1,4 @@
+from .scenarios import LockedScenario, load_locked_v1
+from .scoring import choice_accuracy, distribution_shift
+
+__all__ = ["LockedScenario", "load_locked_v1", "choice_accuracy", "distribution_shift"]
